c8c14fbd30e9185cf5374794d961cf73a5acf07a
Vitalii Maletskyi <vitamal@ukr.net>
2019-08-26 18:56:34 +0300

    next little chenges with get_context_data

--- atelier/views/client_detail_view.py
+++ atelier/views/client_detail_view.py
@@ -1,6 +1,13 @@
-from atelier.models import Client
+from atelier.models import Client, Order
 from django.views import generic
 
 
 class ClientDetailView(generic.DetailView):
     model = Client
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the orders
+        context['order_list'] = Order.objects.all().filter(client=self.object)
+        return context
