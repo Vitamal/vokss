577d606d3f7fe5a94c7159698d0bba4c905ed608
Vitalii Maletskyi <vitamal@ukr.net>
2019-08-29 22:13:29 +0300

    add some .html forms

--- atelier/views/__init__.py
+++ atelier/views/__init__.py
@@ -2,5 +2,7 @@ from .index import *
 from .client_view import *
 from .product_view import *
 from .order_view import *
-
-
+from .minimal_style_view import *
+from .fabric_view import *
+from .complication_elements_view import *
+from .allowance_discount_view import *
