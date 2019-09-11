c12fe9275233a621ce8ae6675f51c210ef122766
vitamal <vitamal@ukr.net>
2019-09-11 16:02:56 +0300

    change the template path directory

--- vokss/settings.py
+++ vokss/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'vokss.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'atelier/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
