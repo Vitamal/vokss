04d22bae6f29421e9d18827b6f2f3395d9c97897
vitamal <vitamal@ukr.net>
2019-10-10 20:54:30 +0300

    write allowance_discount views tests

--- atelier/tests/tests_views.py
+++ atelier/tests/tests_views.py
@@ -1,9 +1,7 @@
-from django.test import RequestFactory, TestCase
+from django.test import TestCase
 from model_mommy import mommy
 from atelier.models import AllowanceDiscount
 from django.urls import reverse_lazy
-from atelier.views import AllowanceDiscountDetailView
-from atelier import views
 
 
 class PagesTest(TestCase):
@@ -43,19 +41,17 @@ class PagesTest(TestCase):
 
 class AllowanceDiscountViewTests(TestCase):
     def setUp(self):
-        """Create an instance"""
+        """Create an instances more than 10 for pagination tests"""
         self.allowance_discount = mommy.make(AllowanceDiscount, _quantity=13)
 
     def test_allowance_discount_detail_view(self):
         instance_id = self.allowance_discount[0].id
-        print(instance_id)
-        response = self.client.get('/en/atelier/allowance_discount/{}/'.format(instance_id))
+        response = self.client.get('/en/atelier/allowance_discount/{}/'.format(instance_id))  # get response in way one
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'atelier/allowance_discount_detail.html')
 
-
     def test_allowance_discount_view_form(self):
-        response = self.client.get(reverse_lazy('atelier:allowance_discount_form'))
+        response = self.client.get(reverse_lazy('atelier:allowance_discount_form'))  # get response in way two
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'atelier/create_form.html')
 
@@ -69,13 +65,18 @@ class AllowanceDiscountViewTests(TestCase):
         response = self.client.get('/en/atelier/allowance_discount/{}/delete/'.format(instance_id))
         self.assertEqual(response.status_code, 200)
 
+    def test_allowance_discount_list_view(self):
+        response = self.client.get('/en/atelier/allowance_discount/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'atelier/allowance_discount_list.html')
+
     def test_pagination_is_ten(self):
-        """ Create more then 10 instances."""
         resp = self.client.get(reverse_lazy('atelier:allowance_discount_list'))
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(len(resp.context['object_list']) == 10)
+
 
 
     # def test_language_using_cookie(self):
