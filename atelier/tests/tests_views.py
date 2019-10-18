8ad9988cffeca0e0385630a30099fef7457dca5f
vitamal <vitamal@ukr.net>
2019-10-18 15:55:11 +0300

    still work with tests: add the ClientView test

--- atelier/tests/tests_views.py
+++ atelier/tests/tests_views.py
@@ -139,20 +139,6 @@ class AllowanceDiscountViewTests(TestCase):
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(len(resp.context['object_list']) == 10)
 
-    # def test_language_using_cookie(self):
-    #     self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'uk'})
-    #     response = self.client.get('/atelier/allowance_discount/')
-    #     self.assertEqual(response.content, b"allowance_discount")
-    #
-    # def test_language_using_header(self):
-    #     response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='fr')
-    #     self.assertEqual(response.content, b"Bienvenue sur mon site.")
-    #
-    # def test_language_using_override(self):
-    #     with translation.override('fr'):
-    #         response = self.client.get('/')
-    #     self.assertEqual(response.content, b"Bienvenue sur mon site.")
-
 
 class ClientViewTests(TestCase):
     def setUp(self):
@@ -201,10 +187,13 @@ class ClientViewTests(TestCase):
         self.assertTrue(len(resp.context['object_list']) == 10)
 
     def test_get_context_data_client(self):
-        order = mommy.make(Order, client=self.clients)
+        """
+            test context['order_list'] for the client view
+        """
         instance_id = self.clients[0].id
+        order = mommy.make(Order, client=self.clients[0])
         response = self.client.get('/en/atelier/client/{}/'.format(instance_id))
-        self.assertEqual(response.context['order_list'], self.clients.order)
+        self.assertEqual(response.context['order_list'][0], order)
 
     # def test_language_using_cookie(self):
     #     self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'uk'})
