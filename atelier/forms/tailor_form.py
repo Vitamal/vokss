680866abd4b3ab85aa1c0749018cba4aa4e9b681
Vitalii Maletskyi <vitamal@ukr.net>
2019-11-30 08:46:30 +0200

    commit before pull (ievv devrun - OK)

--- atelier/forms/tailor_form.py
+++ atelier/forms/tailor_form.py
@@ -5,5 +5,5 @@ from atelier.models import Tailor
 class TailorForm(forms.ModelForm):
     class Meta:
         model = Tailor
-        fields = ('first_name', 'last_name', 'email', 'atelier')
-        # fields = '__all__'
+        # fields = ('first_name', 'last_name', 'email', 'atelier')
+        fields = '__all__'
--- atelier/views/__init__.py
+++ atelier/views/__init__.py
@@ -8,4 +8,3 @@ from .complication_element_view import *
 from .allowance_discount_view import *
 from .atelier_view import *
 from .tailor_view import *
-from .register import *
